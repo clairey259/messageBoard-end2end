9365188da9e05fdd14b489dd375438a49dc3ec9f
Claire <claire.huizenga@outlook.com>
2023-02-07 21:03:05 +1100

    created URL connection

--- messageboard-app/src/components/Message.jsx
+++ messageboard-app/src/components/Message.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Styles from "../App.module.scss";
+import getData, { buildURL } from "../services/data.service";
 
 const Message = (props) => {
     const {message} = props;
+    console.log(getData())
   return (
     <div className= {Styles.message} key={message.id}>
       <h1>{message.name}</h1>
---
+++ messageboard-app/src/services/data.service.js
@@ -0,0 +1,17 @@
+const API_URL = "http://127.0.0.1:3000/posts";
+
+export const buildURL = () => {
+    const url = new URL(API_URL);
+    return url.href
+}
+
+const getData = () => {
+    const url = buildURL()
+    return fetch(url)
+        .then((res) => res.json())
+        //.then((message) => {
+            //return message
+        //})
+}
+
+export default getData
